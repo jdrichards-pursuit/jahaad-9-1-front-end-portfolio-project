e3c0ee185fb0a67b0903c1232164e28f1ac4a658
PESolut <jahaadpetty@pursuit.org>
2022-09-16 00:49:29 -0400

    Navbar revamped, empty about us, projects, pursuit, story | no home page on nav bar

--- main.js
+++ main.js
@@ -41,9 +41,10 @@ fetch(FULL_URL_QUOTES)
     // first the element ( list )
 
     //aboutus
-    const about = document.createElement('a')
-    about.href = 'Pages/aboutUs/index.html' //  # the hash is used to scroll to specific id of a page or top of a page when used by itself
-    about.textContent = 'About Us'
+    // const about = document.createElement('a')
+    // about.href = 'Pages/aboutUs/index.html' //  # the hash is used to scroll to specific id of a page or top of a page when used by itself
+    // about.textContent = 'About Us'
+
     // const homeInput = document.createElement('input')
     // homeInput.type = 'button'
     // homeInput.value = 'HOME'
@@ -55,7 +56,7 @@ fetch(FULL_URL_QUOTES)
     const navBar = document.querySelector('.navbar')
 
     // apending
-    navBar.append(about)
+    // navBar.append(about)
     // starterNavBarItem.append(starterNavBarItemInput)
 
 // Add functionality to the navbar
@@ -65,37 +66,38 @@ navBar.addEventListener('click', event => {
 })
 
 // Populating our NavBar
-    const pageNumber = 4
-    const home = 'Home'
-    const projects = 'Projects'
-    const pursuit = 'Pursuit'
-    const story = 'Story'
-    const homeLink = 'index.html'
-    const projectsLink = 'Pages/projects/index.html'
-    const pursuitLink = 'Pages/pursuit/index.html'
-    const storyLink = 'Pages/story/index.html'
-    const arrOfPages = [home,projects,pursuit,story]
-    const arrOfPagesLinks = [homeLink,projectsLink,pursuitLink,storyLink]
-    console.log(arrOfPages)
-    console.log(arrOfPagesLinks)
-    function populateNav(pageArr,pageLinksArr){ 
-        console.log('test')
-        console.log(pageArr[0])
-    for (let i = 0; i < pageNumber; i++) {
-        // console.log(pageArr[i])
-        const page = pageArr[i]
-        const pageLink = pageLinksArr[i]
-        // console.log(page)
+    // const pageNumber = 4
+    // const home = 'Home'
+    // const projects = 'Projects'
+    // const pursuit = 'Pursuit'
+    // // const story = 'Story'
+    // const homeLink = './index.html'
+    // const projectsLink = 'Pages/projects/index.html'
+    // const pursuitLink = 'Pages/pursuit/index.html'
+    // // const storyLink = 'Pages/story/index.html'
+    // const arrOfPages = [home,projects,pursuit]
+    // const arrOfPagesLinks = [homeLink,projectsLink,pursuitLink]
+    // console.log(arrOfPages)
+    // console.log(arrOfPagesLinks)
+    // function populateNav(pageArr,pageLinksArr){ 
+    //     console.log('test')
+    //     console.log(pageArr[0])
+    // for (let i = 0; i < pageNumber; i++) {
+    //     // console.log(pageArr[i])
+    //     const page = pageArr[i]
+    //     const pageLink = pageLinksArr[i]
+    //     // console.log(page)
 
-        const appPage = document.createElement('a')
-        console.log (appPage)
-        appPage.textContent = page
-        appPage.href = pageLink
+    //     const appPage = document.createElement('a')
+    //     console.log (appPage)
+    //     appPage.textContent = page
+    //     appPage.href = pageLink
         
-        navBar.append(appPage)
+    //     navBar.append(appPage)
         // console.log(pageLink)
         // page = document.createElement('a')
         // page.href = page.arrOfPagesLinks
-    }
-}
-populateNav(arrOfPages,arrOfPagesLinks)
+//     }
+// }
+// populateNav(arrOfPages,arrOfPagesLinks)
+
