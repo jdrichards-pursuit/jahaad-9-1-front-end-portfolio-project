a26b24a098d58f4de4e21aca20f588ace45bdb31
PESolut <jahaadpetty@pursuit.org>
2022-09-27 15:18:41 -0400

    contact us message functionality completed

--- main.js
+++ main.js
@@ -21,12 +21,54 @@ fetch(FULL_URL_QUOTES)
   
 })
 
+// functionality for Contact Us form on aboutUs/index.html
 const messageForm = document.querySelector('.message')
+const messageStorage = document.getElementById('messageStorage')
 console.log(messageForm)
+let logID = 1
 messageForm.addEventListener('submit', event => {
   console.log(`Form has been submitted`)
-  event.preventDefault
+  event.preventDefault()
 
+  // saving our inputs into variables
+  firstNameValue = document.getElementById('fname').value
+  lastNameValue = document.getElementById('lname').value
+  emailValue = document.getElementById('email').value
+  messageValue = document.getElementById('message').value
+  
+  // Logging our inputs
+  console.log(`= = inputs = =`)
+  console.log(firstNameValue,lastNameValue,emailValue,messageValue)
+  console.log(`= = = = = = = =`)
+
+  // creating our article
+  const article = document.createElement('article')
+
+  // populating our article with elements
+
+  // creating our elements
+  const articleFirstName = document.createElement('p')
+  const articleLastName = document.createElement('p')
+  const articleEmail = document.createElement('p')
+  const articleMessage = document.createElement('p')
+  // populating our elements with data from inputs
+  articleFirstName.textContent = firstNameValue
+  articleLastName.textContent = lastNameValue
+  articleEmail.textContent = emailValue
+  articleMessage.textContent = messageValue
+  // appending our elements to article
+  article.append(articleFirstName,articleLastName,articleEmail,articleMessage)
+  // adding a class to our article
+  article.classList.add('messageInput')
+  // adding a unique id to our article
+  article.setAttribute('id', `${logID}`)
+  logID = logID + 1
+  // appending our article to our message box
+  messageStorage.append(article)
+  
+
+
+  //reseting our form
+  messageForm.reset()
+  console.log(`Form has been cleared!`)
 })
-
-console.log(messageForm)
